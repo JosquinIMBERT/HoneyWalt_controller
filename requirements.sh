782d3fea2493f86b681b4369ed420cb8f211d4dd
Josquin IMBERT <josquin.imbert@gmail.com>
2023-03-09 14:45:17 +0100

    Improving requirements

--- requirements.sh
+++ requirements.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# Installing dependencies
 pip3 install sshtunnel
 
+# Adding environment variable
 dir=$(dirname $0)
 home=$(realpath ${dir})
 echo "export HONEYWALT_CONTROLLER_HOME=${home}/" >> ~/.bash_profile
+
+# Include ~/.bash_profile if ~/.bashrc does not exist or if it does not include it already
+if [ ! -f ~/.bashrc ] || ! grep -q \.bash_profile <~/.bashrc; then
+cat <<EOT >> ~/.bashrc
+if [ -f ~/.bash_profile ]; then
+    . ~/.bash_profile
+fi
+EOT
+fi
