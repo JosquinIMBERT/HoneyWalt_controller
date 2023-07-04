447a66d0cbb422a91352d55a2c35a87d8aabfffb
Josquin IMBERT <josquin.imbert@gmail.com>
2023-07-04 12:00:16 +0200

    fixing pid

--- src/honeywalt_controller.py
+++ src/honeywalt_controller.py
@@ -116,7 +116,7 @@ if __name__ == '__main__':
 		path = Path(pid_file_path)
 		if path.parent.exists():
 			with open(pid_file_path, "w") as pid_file:
-				pid_file.write(os.getpid())
+				pid_file.write(str(os.getpid()))
 
 	threading.current_thread().name = "MainThread"
 
