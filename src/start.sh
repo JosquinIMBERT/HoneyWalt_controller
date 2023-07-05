eabaa66ebedec1612e40add0c5587b1cecea088f
Josquin IMBERT <josquin.imbert@gmail.com>
2023-07-05 12:03:32 +0200

    Adding start and stop scripts

--- src/start.sh
+++ src/start.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# This script should be called at boot by the controller
+# Usage: start.sh <log-level> [pidfile]
 
-# Update HoneyWalt
-#git -C ${HONEYWALT_CONTROLLER_HOME} reset --hard
-#git -C ${HONEYWALT_CONTROLLER_HOME} pull
+HOME=$(realpath $(dirname "$0"))
 
-# Start HoneyWalt
-python3 ${HONEYWALT_CONTROLLER_HOME}/src/honeywalt_controller.py
+if [[ $# -lt 1 ]]; then
+	echo "Usage: $0 <log-level> [pidfile]"
+	exit 1
+else
+	loglevel="$1"
+fi
+
+if [[ $# -gt 1 ]]; then
+	pidfile="$1"
+else
+	pidfile="${HOME}/var/honeywalt_controller.pid"
+fi
+
+# Start HoneyWalt_controller
+python3 ${HOME}/src/honeywalt_controller.py \
+	--log-level ${loglevel} \
+	--pid-file ${pidfile}
